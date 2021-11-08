48da8f5f7c485026c9ced5cec8fa85746b908c48
Andre Nguyen <andguen26179@gmail.com>
2021-11-08 23:38:27 +0700

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -20,12 +21,12 @@ namespace API.Controllers
         public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _mapper = mapper;
-            _userRepository = userRepository;      
+            _userRepository = userRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {   
+        {
             var users = await _userRepository.GetMembersAsync();
 
             return Ok(users);
@@ -33,8 +34,23 @@ namespace API.Controllers
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {   
-            return await _userRepository.GetMemberAsync(username);          
+        {
+            return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
